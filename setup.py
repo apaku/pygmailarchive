6b585e68444bddfdd4e936bb9b8e98f82519b41b
Andreas Pakulat <apaku@gmx.de>
2012-05-19 00:04:02 +0200

    Add setuptools support
    Rather basic script only, nothing fancy needed anyway.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = "pygmailarchive",
+        version = "0.1.0",
+        author = "Andreas Pakulat",
+        author_email = "apaku@gmx.de",
+        description = ("An utility to archive Mails from GMail accounts."),
+        license = "Simplified BSD",
+        keywords = "gmail imap archive",
+        url = "https://github.com/apaku/pygmailarchive",
+        install_requires = ["IMAPClient"],
+        scripts = ["pygmailarchive.py"],
+        long_description = read('README'),
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "Environment :: Console",
+            "License :: OSI Approved :: Simplified BSD License",
+        ],
+)
